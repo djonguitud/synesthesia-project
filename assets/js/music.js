32d5ac31e3f2366a07ea7d3024125b7e3a8816d2
JorgeRamirezAnzaldo <112900820+JorgeRamirezAnzaldo@users.noreply.github.com>
2022-10-26 17:08:57 -0500

    Find artists-track (#26)
    Co-authored-by: Julio C. MW11 <sysa.mail@gmail.com>

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,23 +1,20 @@
-var cors = "https://cors-anywhere.herokuapp.com/";
-var apikey = "9ffbe6147c3d611e3a0048e57074efe8";
-
-var trackUrlMusix = cors + "https://api.musixmatch.com/ws/1.1/track.search?apikey="+apikey+"&f_has_lyrics=1&q_track="
-
-var lyricsUrlMusix = cors +"https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey="+apikey+"&track_id=";
-
-
-var trackIdMusix ;
+var cors            = "https://cors-anywhere.herokuapp.com/";
+var apikey          = "9ffbe6147c3d611e3a0048e57074efe8";
+var trackUrlMusix   = cors + "https://api.musixmatch.com/ws/1.1/track.search?apikey="+apikey+"&f_has_lyrics=1&q_track="
+var lyricsUrlMusix  = cors +"https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey="+apikey+"&track_id=";
+var trackIdMusix;
 var lyricsBody;
 
-
-function getLirycs(trackName)
+/*
+function getLirycs(trackName, TrackArtistName)
 {
-    var trackUrl = trackUrlMusix +trackName;
+    var trackUrl = trackUrlMusix +trackName+"&"+TrackArtistName;
 
     fetch(trackUrl)
     .then(function (response)  {
     return response.json();
     }).then (function(data){ 
+        console.log("---track---")
         console.log(data);
         console.log(data.message.body.track_list[0].track.track_id);
         trackIdMusix = data.message.body.track_list[0].track.track_id;
@@ -30,6 +27,7 @@ function getLirycs(trackName)
         .then(function (response)  {
         return response.json();
         }).then (function(data){ 
+            console.log("---lyrics---")
             console.log(data);
             lyricsBody = data.message.body.lyrics.lyrics_body;
             console.log(lyricsBody);
@@ -40,15 +38,50 @@ function getLirycs(trackName)
         });
 
     } );
-
     return lyricsBody;
 }
+*/
 
-
-function init()
-{
-        //lirycsdisplay
-        getLirycs("lamento boliviano");
+async function getTrackAsync (trackName, TrackArtistName){
+    var requestUrl = trackUrlMusix +trackName+"&q_artist="+TrackArtistName;
+    var response = await fetch(requestUrl);
+    var data = await response.json();
+    console.log("---trackAsync---");
+    console.log(data);
+    return data;
 }
 
-//init ();
+async function getDataLirycsAsyc (trackIdMusixParm){
+    var requestUrl = lyricsUrlMusix + trackIdMusixParm;
+    var response = await fetch(requestUrl);
+    var data = response.json();
+    console.log("---dataLyricAsync---");
+    console.log(data);
+    return data;
+}
+
+async function getLirycsAsyc(trackName, TrackArtistName){
+    trackIdMusix = null;
+    var dataTrack = await getTrackAsync(trackName, TrackArtistName);
+    if (dataTrack !== null && dataTrack.message.header.status_code == 200 && dataTrack.message.body.track_list.length > 0){
+        debugger
+        trackIdMusix = dataTrack.message.body.track_list[0].track.track_id;        
+        if (trackIdMusix !== null ){
+            var dataLirycs = await getDataLirycsAsyc(trackIdMusix)
+            if (dataLirycs !== null && dataLirycs.message.header.status_code == 200){
+                lyricsBody = dataLirycs.message.body.lyrics.lyrics_body;
+                console.log(lyricsBody);                
+                $("#lyricsMusixmatch").text(lyricsBody);
+            }
+            else{
+                $("#lyricsMusixmatch").text("No Track - Lyrics found for : "  + trackName + "/"+TrackArtistName);
+            }
+        }
+        else{
+            $("#lyricsMusixmatch").text("No Lyrics found for : "  + trackName + "/"+TrackArtistName);
+        }
+    }
+    else{
+        $("#lyricsMusixmatch").text("No Track found for : "  + trackName + "/"+TrackArtistName);
+    }
+}
