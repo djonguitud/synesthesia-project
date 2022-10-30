4f11a90d1c25a60f6d3b0b81c34b144d9ded2289
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2022-10-30 09:26:07 -0600

    Removing Automatic CORS attempt

--- assets/js/music.js
+++ assets/js/music.js
@@ -26,23 +26,6 @@ var lyricsBody;
  */
 function getLirycs(trackName, TrackArtistName){
 
-    (function() {
-        var cors_api_host = 'cors-anywhere.herokuapp.com';
-        var cors_api_url = 'https://' + cors_api_host + '/';
-        var slice = [].slice;
-        var origin = window.location.protocol + '//' + window.location.host;
-        var open = XMLHttpRequest.prototype.open;
-        XMLHttpRequest.prototype.open = function() {
-            var args = slice.call(arguments);
-            var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
-            if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
-                targetOrigin[1] !== cors_api_host) {
-                args[1] = cors_api_url + args[1];
-            }
-            return open.apply(this, args);
-        };
-    })();
-
     trackIdMusix= null;
     lyricsBody  = null;
     var trackUrl = trackUrlMusix +trackName+"&q_artist="+TrackArtistName;
